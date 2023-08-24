e724104a9e52e5df731e21ebd4be49f964314c64
Timo <beckmann.timo@icloud.com>
2023-08-24 15:50:39 +0000

    home nav link & view h1 styling

--- resources/views/components/header.blade.php
+++ resources/views/components/header.blade.php
@@ -3,6 +3,11 @@
         <div class="navbar-start">
             <p class="text-xl">Video Management System</p>
         </div>
+        <div class="navbar-center">
+            <a class="navbar-item" href="/">Home</a>
+            <a class="navbar-item" herf="/premium">Premium</a>
+            <a class="navbar-item" herf="/contact">Contact</a>
+        </div>
         <div class="navbar-end">
             <a class="btn btn-primary" href="/upload">
                 Upload
--- resources/views/view.blade.php
+++ resources/views/view.blade.php
@@ -1,6 +1,8 @@
 @props(['video'])
 
-<x-layout title="View Video">
-    <h1>{{ $video->title }}</h1>
-    <p>{{ $video->description }}</p>
+<x-layout title="{{ $video->title }} / VidMS / Simple Video CMS" :description="$video->description">
+    <section class="my-12">
+        <h1 class="text-4xl text-content1 mb-2">{{ $video->title }}</h1>
+        <p class="text-content2">{{ $video->description }}</p>
+    </section>
 </x-layout>
