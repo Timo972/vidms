aa5bebc8d6fefc06802abbb343749f9de393b2e2
tbeck <beckmann.timo@icloud.com>
2023-09-13 17:54:01 +0200

    fix: vidstack source

--- resources/views/view.blade.php
+++ resources/views/view.blade.php
@@ -12,30 +12,13 @@
     </section>
     <media-player
         title="{{ $video->title }}"
-        src="/video/{{ $video->slug }}"
-        type="{{ $mime }}"
         poster="https://image.mux.com/VZtzUzGRv02OhRnZCxcNg49OilvolTqdnFLEqBsTwaxU/thumbnail.webp?time=268&width=980"
         thumbnails="https://media-files.vidstack.io/sprite-fight/thumbnails.vtt"
         aspect-ratio="16/9"
         crossorigin
     >
         <media-outlet>
-            <media-poster
-                alt="Girl walks into sprite gnomes around her friend on a campfire in danger!"
-            ></media-poster>
-            <track
-                src="https://media-files.vidstack.io/sprite-fight/subs/english.vtt"
-                label="English"
-                srclang="en-US"
-                kind="subtitles"
-                default
-            />
-            <track
-                src="https://media-files.vidstack.io/sprite-fight/chapters.vtt"
-                srclang="en-US"
-                kind="chapters"
-                default
-            />
+            <source src="/video/{{ $video->slug }}" type="{{ $mime }}" />
         </media-outlet>
         <media-community-skin></media-community-skin>
     </media-player>
